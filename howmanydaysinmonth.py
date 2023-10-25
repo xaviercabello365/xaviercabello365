40f79516395e2c7625f08f3188350cd760a0329d
xavier.cabello.7e7 <xavier.cabello.7e7@itb.cat>
2023-10-25 12:26:03 +0200

    exercici3

---
+++ howmanydaysinmonth.py
@@ -0,0 +1,30 @@
+"""
+Xavier Cabello
+UF1 MO1
+25/10/2023
+"""
+#demanem un numero per sapiguer els dies de qualsevol mes de l'any
+mes = int(input("Ingresa el número del mes (1-12): "))
+
+dias_por_mes = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
+
+
+if mes >= 1 and mes <= 12:
+    numero_de_dias = dias_por_mes[mes]
+    print(f"El mes {mes} tiene {numero_de_dias} días.")
+else:
+    print("Número de mes inválido. Debe estar entre 1 y 12.")
+
